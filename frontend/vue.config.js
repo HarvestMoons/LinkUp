a2274d6606eae5d6a8655af2ca05f15b12664756
陈星宇 <125285397+HarvestMoons@users.noreply.github.com>
2025-02-05 04:58:07 +0800

    1.4.1_chore: change frontend port to 3000, backend port to 8099

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,9 +1,10 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   devServer: {
+    port: 3000, // 将前端端口改为 3000
     proxy: {
       '/api': {
-        target: "http://localhost:8080/",
+        target: "http://localhost:8099/", // 后端端口 8099
         changeOrigin: true,
         pathRewrite: {
           '^/api': ''
